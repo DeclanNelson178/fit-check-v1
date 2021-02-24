97903b373f71cf51539d7b9aca82ca2beb5a1998
Emily Costello <emcostello2018@gmail.com>
2021-02-23 23:24:33 -0500

    Replacing modal with signup screen component.

--- client/src/components/SignUp/SignUp.js
+++ client/src/components/SignUp/SignUp.js
@@ -1,5 +1,7 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "./SignUp.css";
+import mailIcon from "./images/mail_outline.png";
+import keyIcon from "./images/vpn_key.png";
 import axios from "axios";
 
 /**
@@ -10,162 +12,143 @@ import axios from "axios";
  * @author [Emily Costello] (https://github.com/ecostello9)
  */
 class SignUp extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      email: "",
-      password: "",
-      passwordC: "",
-    };
-    this.handleSignUp = this.handleSignUp.bind(this);
-    this.handleNameChange = this.handleNameChange.bind(this);
-    this.handleEmailChange = this.handleEmailChange.bind(this);
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
-    this.handleConfirmPasswordChange = this.handleConfirmPasswordChange.bind(
-      this
-    );
-  }
-
-  // Axios is used to handle requests to backend endpoints --> Here we use a POST request
-  // with the user's inputted name, email and password
-  handleSignUp() {
-    // axios
-    // .post("http://localhost:5000/auth/signin", {
-    //     email: this.state.username,
-    //     password: this.state.password,
-    // })
-    // .then((res) => {
-    //     if (res.status === 200) {
-    //       this.props.data(res.data.token);
-    //     } else {
-    //       // this.state.incorrectLogin = true;
-    //     }
-    // });
-  }
-
-  // Updates dynamically when user inputs email
-  async handleNameChange(e) {
-    console.log(e.target.value);
-    const oldEmail = this.state.email;
-    const oldPassword = this.state.password;
-    const oldConfirmPass = this.state.passwordC;
-    await this.setState({
-      name: e.target.value,
-      email: oldEmail,
-      password: oldPassword,
-      passwordC: oldConfirmPass,
-    });
-  }
-
-  // Updates dynamically when user inputs password
-  async handleEmailChange(e) {
-    const oldName = this.state.name;
-    console.log(e.target.value);
-    const oldPassword = this.state.password;
-    const oldConfirmPass = this.state.passwordC;
-    await this.setState({
-      name: oldName,
-      email: e.target.value,
-      password: oldPassword,
-      passwordC: oldConfirmPass,
-    });
-  }
-
-  // Updates dynamically when user inputs email
-  async handlePasswordChange(e) {
-    const oldName = this.state.name;
-    const oldEmail = this.state.email;
-    console.log(e.target.value);
-    const oldConfirmPass = this.state.passwordC;
-    await this.setState({
-      name: oldName,
-      email: oldEmail,
-      password: e.target.value,
-      passwordC: oldConfirmPass,
-    });
-  }
-
-  // Updates dynamically when user inputs password
-  async handleConfirmPasswordChange(e) {
-    const oldName = this.state.name;
-    const oldEmail = this.state.email;
-    const oldPassword = this.state.password;
-    console.log(e.target.value);
-    await this.setState({
-      name: oldName,
-      email: oldEmail,
-      password: oldPassword,
-      passwordC: e.target.value,
-    });
-    if (this.state.password != this.state.passwordC) {
-      return (
-        <label className="password-match-error">Passwords do not match</label>
-      );
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "",
+            email: "",
+            password: "",
+            passwordC: ""
+        };
+        this.handleSignUp = this.handleSignUp.bind(this);
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleConfirmPasswordChange = this.handleConfirmPasswordChange.bind(this);
     }
-  }
 
-  render() {
-    return (
-      <div className="screen">
-        <div id="signUpModal">
-          <div className="modal-content">
-            <span className="close">&times;</span>
-            <br></br>
-            <br></br>
-            <span id="e94_25">Name</span>
-            <br></br>
-            <div id="e94_23">
-              <input
-                placeholder="Johhny Appleseed"
-                className="signup-name"
-                value={this.state.name}
-                onChange={this.handleNameChange}
-              ></input>
+    // Axios is used to handle requests to backend endpoints --> Here we use a POST request
+    // with the user's inputted name, email and password
+    handleSignUp() {
+        console.log("signed up!");
+    }
+
+    // Updates dynamically when user inputs email
+    async handleNameChange(e) {
+        console.log(e.target.value);
+        const oldEmail = this.state.email;
+        const oldPassword = this.state.password;
+        const oldConfirmPass = this.state.passwordC;
+        await this.setState({name: e.target.value, email: oldEmail, password: oldPassword, passwordC: oldConfirmPass});
+    }
+
+    // Updates dynamically when user inputs password
+    async handleEmailChange(e) {
+        const oldName = this.state.name;
+        console.log(e.target.value);
+        const oldPassword = this.state.password;
+        const oldConfirmPass = this.state.passwordC;
+        await this.setState({name: oldName, email: e.target.value, password: oldPassword, passwordC: oldConfirmPass});
+    }
+
+    // Updates dynamically when user inputs email
+    async handlePasswordChange(e) {
+        const oldName = this.state.name;
+        const oldEmail = this.state.email;
+        console.log(e.target.value);
+        const oldConfirmPass = this.state.passwordC;
+        await this.setState({name: oldName, email: oldEmail, password: e.target.value, passwordC: oldConfirmPass});
+    }
+
+    // Updates dynamically when user inputs password
+    async handleConfirmPasswordChange(e) {
+        const oldName = this.state.name;
+        const oldEmail = this.state.email;
+        const oldPassword = this.state.password;
+        console.log(e.target.value);
+        await this.setState({name: oldName, email: oldEmail, password: oldPassword, passwordC: e.target.value});
+        if (this.state.password != this.state.passwordC) {
+            return (
+                <label className="password-match-error">Passwords do not match</label>
+            );
+        }
+    }
+
+    render() {
+        return (
+            <div className="screen">
+                <div id="e86_14">
+                    <div id="e86_15">
+                        <input placeholder="japple123@email.com" className="signup-input"
+                            value={
+                                this.state.email
+                            }
+                            onChange={
+                                this.handleEmailChange
+                        }></input>
+                    </div>
+                    <div id="e86_16">
+                        <input placeholder="********" className="signup-input"
+                            value={
+                                this.state.password
+                            }
+                            onChange={
+                                this.handlePasswordChange
+                        }></input>
+                    </div>
+                    <span id="e86_17">Password</span>
+                    <span id="e86_18">Email</span>
+                    <div id="e86_19">
+                        <img alt="" className="mail"
+                            src={keyIcon}/>
+                    </div>
+                    <div id="e86_20">
+                        <img alt="" className="mail"
+                            src={mailIcon}/>
+                    </div>
+                    <div id="e103_18">
+                        <input placeholder="Johhny Appleseed" className="signup-input"
+                            value={
+                                this.state.name
+                            }
+                            onChange={
+                                this.handleNameChange
+                        }></input>
+                    </div>
+                    <span id="e103_19">Name</span>
+                    <div id="e86_22">
+                        <button id="signup_button"
+                            onClick={
+                                this.handleSignUp
+                        }></button>
+                    </div>
+                    <span id="e86_23">your pocket stylist</span>
+                    <div id="e86_25">
+                        <span id="ei86_25_43_16">“FIT CHECK”</span>
+                    </div>
+                    <div id="e87_0">
+                        <input placeholder="********" className="signup-input"
+                            value={
+                                this.state.passwordC
+                            }
+                            onChange={
+                                this.handleConfirmPasswordChange
+                        }></input>
+                    </div>
+                    <div id="e87_2">
+                        <img alt="" className="mail"
+                            src={keyIcon}/>
+                    </div>
+                    <span id="e91_22">SIGN UP</span>
+                    <span id="e91_24">Confirm Password</span>
+                    {
+                    this.state.password == this.state.passwordC ? <div></div> : <span id="e91_26">Passwords do not match!</span>
+                }
+                    <div id="e104_15"></div>
+                </div>
             </div>
-            <br></br>
-            <span id="e94_4">Email</span>
-            <br></br>
-            <div id="e94_1">
-              <input
-                placeholder="japple123@email.com"
-                className="signup-email"
-                value={this.state.email}
-                onChange={this.handleEmailChange}
-              ></input>
-            </div>
-            <br></br>
-            <span id="e94_3">Password</span>
-            <br></br>
-            <div id="e94_2">
-              <input
-                placeholder="********"
-                className="signup-password"
-                value={this.state.password}
-                onChange={this.handlePasswordChange}
-              ></input>
-            </div>
-            <br></br>
-            <span id="e94_19">Confirm Password</span>
-            <br></br>
-            <div id="e94_14">
-              <input
-                placeholder="********"
-                className="signup-passwordC"
-                value={this.state.passwordC}
-                onChange={this.handleConfirmPasswordChange}
-              ></input>
-            </div>
-            <br></br>
-            <span id="e94_18">SIGN UP</span>
-            <div id="e94_8">
-              <button id="signup_button" onClick={this.handleSignUp}></button>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default SignUp;
