998a18e27cdb2fcf858435432a49d6848a0a23c1
Matthew Carmona-Gonzalez <mcg6@gatech.edu>
2021-03-04 17:38:39 -0500

    Updated AI to return attributes and categories

--- AI/app.py
+++ AI/app.py
@@ -9,14 +9,15 @@ from mmcv import Config
 from mmcv.runner import load_checkpoint
 
 from mmfashion.core import AttrPredictor
+from mmfashion.core import CatePredictor
 from mmfashion.models import build_predictor
 from mmfashion.utils import get_img_tensor
 
 class FitCheckPredictor:
 	def __init__(self, args):
 		self.args = {
-			"checkpoint":"checkpoint/Predict/vgg/global/latest.pth",
-			"config":"configs/attribute_predict/global_predictor_vgg_attr.py",
+			"checkpoint":"checkpoint/CateAttrPredict/vgg/global/latest.pth",
+			"config":"configs/category_attribute_predict/global_predictor_vgg.py",
 			"use_cuda":False
 		}
 	def build_model(self):
@@ -25,33 +26,50 @@ class FitCheckPredictor:
 		# global attribute predictor will not use landmarks
 		# just set a default value
 		self.landmark_tensor = torch.zeros(8)
-		self.cfg.model.pretrained = None
+
 		self.model = build_predictor(self.cfg.model)
 		load_checkpoint(self.model, self.args["checkpoint"], map_location='cpu')
+		print('model loaded from {}'.format(self.args["checkpoint"]))
 		if self.args["use_cuda"]:
 		    self.model.cuda()
 		    self.landmark_tensor = self.landmark_tensor.cuda()
 
 		self.model.eval()
-	def get_image_attr(self, img):
-		img_tensor = get_img_tensor(img, self.args["use_cuda"])
-		# predict probabilities for each attribute
-		attr_prob = self.model(
-		    img_tensor, attr=None, landmark=self.landmark_tensor, return_loss=False)
-		attr_predictor = AttrPredictor(self.cfg.data.test)
-
-		if isinstance(attr_prob, torch.Tensor):
-		        data = attr_prob.data.cpu().numpy()
-		elif isinstance(attr_prob, np.ndarray):
-		    data = attr_prob
-
+	def get_json_attr(self, prob, predictor, top):
+		if isinstance(prob, torch.Tensor):
+			data = prob.data.cpu().numpy()
+		elif isinstance(prob, np.ndarray):
+			data = prob
 		indexes = np.argsort(data[0])[::-1]
 		top = 10
-		retVal = {"attributes": [
-		]}
+		retVal = []
 		for i in indexes[:top]:
-			retVal["attributes"].append({"name":attr_predictor.attr_idx2name[i], "probability":float(data[0][i])})
-		return json.dumps(retVal) 
+			retVal.append({"name":predictor.attr_idx2name[i], "probability":float(data[0][i])})
+		return retVal
+		
+	def get_json_cate(self, prob, predictor, top):
+		if isinstance(prob, torch.Tensor):
+			data = prob.data.cpu().numpy()
+		elif isinstance(prob, np.ndarray):
+			data = prob
+		indexes = np.argsort(data[0])[::-1]
+		top = 10
+		retVal = []
+		for i in indexes[:top]:
+			retVal.append({"name":predictor.cate_idx2name[i], "probability":float(data[0][i])})
+		return retVal
+
+	def get_image_results(self, img):
+		img_tensor = get_img_tensor(img, self.args["use_cuda"])
+		# predict probabilities for each attribute/category
+		attr_prob, cate_prob = self.model(
+			img_tensor, attr=None, landmark=self.landmark_tensor, return_loss=False)
+		attr_predictor = AttrPredictor(self.cfg.data.test)
+		cate_predictor = CatePredictor(self.cfg.data.test)
+		attributes = self.get_json_attr(attr_prob, attr_predictor, 10)
+		categories = self.get_json_cate(cate_prob, cate_predictor, 10)
+		return json.dumps({"attributes":attributes, "categories":categories})
+
 
 predictor = FitCheckPredictor(sys.argv)
 predictor.build_model()
@@ -59,11 +77,11 @@ predictor.build_model()
 
 
 app = flask.Flask(__name__)
-@app.route("/get-attributes", methods=['POST'])
+@app.route("/get-analysis", methods=['POST'])
 def home(): # route handler function
 	# returning a response
 	filename = flask.request.get_json(silent=True)['filename']
-	res = flask.Response(predictor.get_image_attr(filename))
+	res = flask.Response(predictor.get_image_results(filename))
 	res.headers['Content-Type'] = "application/json"
 	return res
 
