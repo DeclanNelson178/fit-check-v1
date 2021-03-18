bd20ea78ea72e68d08517c6036bf1d575ceb1281
Abhinav Joshi <abhinavmjoshi@gmail.com>
2021-03-18 19:12:03 -0400

    finished restyling home screen

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -29,17 +29,10 @@ class Home extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div id="e33_3">
-          <Link
-            to={{
-              pathname: "/getadvice",
-              state: {
-                jwt: this.state.jwt,
-              },
-            }}
-          >
-            <div id="e47_6"></div>
-          </Link>
+        <div class="e33_3">
+          <div class="e136_1980"></div>
+          <div class="e136_1983"></div>
+          <div class="e136_1982"></div>
           <Link
             to={{
               pathname: "/getinspired",
@@ -48,20 +41,9 @@ class Home extends Component {
               },
             }}
           >
-            <div id="e47_20"></div>
+            <span class="e47_21">// GET INSPIRED</span>
           </Link>
-          <Link
-            to={{
-              pathname: "/getinspired",
-              state: {
-                jwt: this.state.jwt,
-              },
-            }}
-          >
-            <span id="e47_21">
-              GET<br></br>INSPIRED
-            </span>
-          </Link>
+          <span class="e136_1975">// COMMUNITY</span>
           <Link
             to={{
               pathname: "/getadvice",
@@ -70,25 +52,19 @@ class Home extends Component {
               },
             }}
           >
-            <span id="e47_22">
-              GET<br></br>ADVICE
-            </span>
+            <span class="e47_22">// GET ADVICE</span>
           </Link>
-          <div id="e46_7">
-            <div id="e44_7"></div>
-            <div id="e44_3"></div>
-            <div id="e44_4"></div>
-            <span id="e46_6">“FIT CHECK”</span>
-            <div id="e48_1"></div>
-            <div id="e46_9">
-              <input
-                id="e46_9_input"
-                placeholder=" what u tryna wear?"
-                //value={}
-                onChange={this.handleSearch}
-              ></input>
-            </div>
+          <div class="e78_0"></div>
+          <div class="e82_14"></div>
+          <div class="e136_1974"></div>
+          <div class="e145_7"></div>
+          <div class="e145_8"></div>
+          <div class="e136_2000">
+            <div class="e136_2001"></div>
+            <div class="e136_2002"></div>
+            <span class="e136_2003">// FIT CHECK</span>
           </div>
+          <div id="e158_1"></div>
         </div>
         <Switch>
           <Route path="/getinspired" component={GetInspired} />
